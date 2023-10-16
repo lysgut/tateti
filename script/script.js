23d4f758e63f4a3ecf3da318fa029cb81984ca67
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2023-10-16 00:31:39 -0300

    listo

--- script/script.js
+++ script/script.js
@@ -1,5 +1,31 @@
-const botones = [document.getElementById("t1"), document.getElementById("t2"),document.getElementById("t3"),document.getElementById("t4"),document.getElementById("t5"),document.getElementById("t6"),document.getElementById("t7"),document.getElementById("t8"),document.getElementById("t9")]
+const xo = ["X", "O"]
+const xo1 = xo[document.getElementById("simboloJugador1").value]
+const xo2 = xo[1 - document.getElementById("simboloJugador1").value]
 
+function continuarJugador1() {
+    popupJugador1.style.display = "none";
+    popupJugador2.style.display = "block";
+}
+
+
+
+function continuarJugador2() {
+    const jugador1 = document.getElementById("nombreJugador1").value;
+    const jugador2 = document.getElementById("nombreJugador2").value;
+    console.log(jugador1 + " = " + xo1)
+    console.log(jugador2 + " = " + xo2)
+    popupJugador2.style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const popupJugador1 = document.getElementById("popupJugador1");
+    const popupJugador2 = document.getElementById("popupJugador2");
+
+    popupJugador1.style.display = "block";
+});
+
+const botones = [document.getElementById("t1"), document.getElementById("t2"),document.getElementById("t3"),document.getElementById("t4"),document.getElementById("t5"),document.getElementById("t6"),document.getElementById("t7"),document.getElementById("t8"),document.getElementById("t9")]
+const msg = document.getElementById("msg")
 
 let player = 0
 
@@ -9,14 +35,25 @@ let player = 0
 
             botones[i].disabled = true
             if (player == 0) {
-                botones[i].textContent = "X"
-                console.log('Celda ' + i2 + " para X");
+                botones[i].textContent = xo1
+                console.log('Celda ' + i2 + " para " + xo1);
+                msg.textContent = "Jugando " + document.getElementById("nombreJugador2").value + "( " +xo2+ " )"
+
             } else {
-                botones[i].textContent = "O"
-                console.log('Celda ' + i2 + " para O");
+                botones[i].textContent = xo2
+                console.log('Celda ' + i2 + " para " + xo2);
+                msg.textContent = "Jugando " + document.getElementById("nombreJugador1").value + "( " +xo1+ " )"
             }
             player = 1-player
-            checkWinner()
+            if(checkWinner()){
+                for(e of botones){
+                    e.disabled=true
+                }
+            } else if(checkEmpate()) {
+                for(e1 of botones){
+                    e1.disabled=true
+                }
+            }
             }
         );;
 
@@ -24,17 +61,16 @@ let player = 0
 
     function checkEmpate()
         {
-            let countBlanks = 0
-            for(e of botones){
-                if(e.textContent=== "")
-                {countBlanks++}
+            let countXO = 0
+            for(j of botones)
+            if(j.textContent == xo1 || j.textContent == xo2){
+                countXO++
             }
-            if(countBlanks === 0){
-                console.log("Empate!! No hay ganadores")
-                for(r of botones){
-                    r.disabled = true
-                }
+            if(countXO == 9){
+                msg.textContent = "Empate !! No hay ganadores."
+                return true
             }
+            return false
         }
     
     function checkWinner(){
@@ -42,14 +78,21 @@ let player = 0
             [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
         ]
         for(elem of winCode){
-            if(botones[elem[0]].textContent === "X" || botones[elem[0]].textContent === "O"){
-            if(botones[elem[0]].textContent === botones[elem[1]].textContent && botones[elem[0]].textContent === botones[elem[2]].textContent){
-                console.log("Jugador " + botones[elem[0]].textContent + " Ha Ganado")
-                for(elem of botones){
-                    elem.disabled = true
-                }}
-                return true
-            }
+            let a = botones[elem[0]].textContent
+            let b = botones[elem[1]].textContent
+            let c = botones[elem[2]].textContent
+            let xos = [xo1, xo2]
+            for(k of xos){
+                if(a==k && b==k && c==k){
+                    if(k == xo1){
+                    msg.textContent = document.getElementById("nombreJugador1").value + " ha ganado ( "+ k +" )"
+                }
+                else{
+                    msg.textContent = document.getElementById("nombreJugador2").value + " ha ganado ( " + k + " )"
+                }
+                    return true
+                }
+            }   
         }
     }
 
